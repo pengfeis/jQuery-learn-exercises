40448d66e00e58e4b38e287eaa4e55e51acda7ea
Pengfei <supengfei007@gmail.com>
2016-10-29 20:34:22 +0800

    add foreach syntax

---
+++ github-project/statics/js/lxf.js
@@ -0,0 +1,10 @@
+var o = {
+    name: 'Jack',
+    age: 20,
+    city: 'Beijing'
+};
+for (var key in o) {
+    if (o.hasOwnProperty(key)) {
+        console.info(key); // 'name', 'age', 'city'
+    }
+}
